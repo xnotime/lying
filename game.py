c62ab3ade3264c3c511d945ff1aed2acd3ee37fa
residualentropy <161914158+residualentropy@users.noreply.github.com>
2024-10-18 12:52:28 -0500

    hell-yeah

--- game.py
+++ game.py
@@ -1,24 +1,29 @@
 from dataclasses import dataclass
 from random import randrange
+import time
 import typing as t
 
 @dataclass
 class Speaker:
     ne: str
     wlink: str
-    desc: str = None
+    desc: str | None = None
 
 class Game:
     code: str
     judge: str
     speakers: t.List[Speaker]
     chosen: int | None # None if game hasn't started yet
+    start_time: float
+    live: bool
 
     def __init__(self, code: str, judge_nick_enc: str):
         self.code = code
         self.judge = judge_nick_enc
         self.speakers = []
         self.chosen = None
+        self.time_left = 10
+        self.live = False
     
     def add_speaker(self, new_speaker: Speaker):
         self.speakers.append(new_speaker)
@@ -26,6 +31,7 @@ class Game:
     def start_game(self):
         # Choose a speaker who will tell the truth
         self.chosen = randrange(0, len(self.speakers))
+        self.start_time = time.time()
     
     def chosen_article(self):
         if self.chosen is None:
@@ -35,4 +41,10 @@ class Game:
     def status(self):
         if self.chosen is None:
             return '@not-started'
-        return 'okey dokey'
+        if self.live:
+            return '@live'
+        now = time.time()
+        time_left = 10 - int(now - self.start_time)
+        if time_left <= 0:
+            self.live = True
+        return f'{time_left}s left'
